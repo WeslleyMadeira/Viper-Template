067f9d1d4a26333c7702f6d2cdb470ce117ec054
Weslley Madeira <weslley.dev@gmail.com>
2019-04-09 14:45:48 -0300

    Initial Commit

---
+++ Viper TemplateTests/Viper_TemplateTests.swift
@@ -0,0 +1,34 @@
+//
+//  Viper_TemplateTests.swift
+//  Viper TemplateTests
+//
+//  Created by Weslley Madeira on 09/04/19.
+//  Copyright © 2019 Wood Project. All rights reserved.
+//
+
+import XCTest
+@testable import Viper_Template
+
+class Viper_TemplateTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
